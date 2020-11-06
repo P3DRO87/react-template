7e3f0de7a202ce0720d4be5921cdae61b94bd5e4
Pedro <gokujose111@gmail.com>
2020-11-06 15:48:02 -0600

    testing bootstrap

--- src/components/Test.js
+++ src/components/Test.js
@@ -3,8 +3,12 @@ import React from "react";
 class Test extends React.Component {
   render() {
     return (
-      <div className="main">
-        <h1>hola mundo</h1>
+      <div className="container-fluid main">
+        <div className="row">
+          <div className="col-sm-auto">
+            <h1 className="text-center">hola mundo</h1>
+          </div>
+        </div>
       </div>
     );
   }
