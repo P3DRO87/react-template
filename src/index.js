f96ec305d6fdf71febca73ea3367f8ea0da333c8
Pedro <gokujose111@gmail.com>
2020-11-06 15:01:29 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.scss";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// rendering components
+import Badge from "./components/Badge";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const main = document.getElementById("app");
+
+//es importannte pasarle como parametro un elemento (<element />) a reactDom.render
+ReactDOM.render(<Badge />, main); // recibe dos argumentos "que elemento"
+// "donde" tiene que aparecer el elemento
