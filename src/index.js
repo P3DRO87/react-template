15c3012397d48a37946aef39a10e97f1a0195420
Pedro <gokujose111@gmail.com>
2021-02-09 00:47:02 -0600

    updating to bootstrap v5

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 // rendering components
 import Example from "./components/Example";
