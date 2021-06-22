5e55ab00e815022dc38a0a31dfc6a739d078eaa8
Pedro <gokujose111@gmail.com>
2021-06-21 19:38:16 -0600

    adding react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
+import { HashRouter, Switch, Route } from "react-router-dom";
+
 import Example from "./components/Example";
+import Example2 from "./components/Example2";
 
 const App = () => {
   return (
-    <>
-      <Example />
-    </>
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" component={Example} />
+        <Route exact path="/example" component={Example2} />
+      </Switch>
+    </HashRouter>
   );
 };
 
