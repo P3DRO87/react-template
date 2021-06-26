9590c4ff32e556df85e3674676efc20f91699a5c
Pedro <gokujose111@gmail.com>
2021-06-26 14:20:30 -0600

    adding react strict mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import React from "react";
 
-import { HashRouter, Switch, Route } from "react-router-dom";
-
 import Example from "./components/Example";
-import Example2 from "./components/Example2";
 
 const App = () => {
-  return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/" component={Example} />
-        <Route exact path="/example2" component={Example2} />
-      </Switch>
-    </HashRouter>
-  );
+  return <Example />;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.scss";
 import App from "./App";
 
-const main = document.getElementById("app");
+const root = document.getElementById("app");
 
-ReactDOM.render(<App />, main);
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  root
+);
