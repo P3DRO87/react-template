8da409436ba6b04d0bc1010e0774a8e98a3a38ba
Pedro <gokujose111@gmail.com>
2021-12-23 17:09:00 -0600

    fixing whatever

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from "react";
-
-import Example from "./components/Example";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  return <Example />;
+   const [count, setCount] = useState(0);
+
+   useEffect(() => {
+      let isMounted = true;
+
+      setInterval(() => {
+         if (isMounted) setCount(count + 1);
+      }, 1000);
+
+      return () => (isMounted = false);
+   }, [count]);
+
+   return <h2>{count}</h2>;
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "bundles"),
@@ -35,6 +36,7 @@ module.exports = {
       },
     ],
   },
+
   resolve: {
     extensions: ["*", ".js", ".jsx"],
     modules: [path.join(__dirname, "src"), "node_modules"],
